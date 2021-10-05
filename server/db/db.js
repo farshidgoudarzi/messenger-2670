266d96dd7f7b6d9b8ba23032894ded7322d0dd63
Farshid Goudarzi <farshid.goudarzi@gmail.com>
2021-10-05 10:23:53 +0300

    initializing done

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,11 @@
 const Sequelize = require("sequelize");
+const dotenv = require('dotenv');
+dotenv.config();
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_name, process.env.DATABASE_username, process.env.DATABASE_password, {
+  host: process.env.DATABASE_host,
+  port: process.env.DATABASE_port,
+  dialect: 'postgres',
   logging: false
 });
 
