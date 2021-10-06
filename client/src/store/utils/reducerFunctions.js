4eec2bd2ed10d51503e3779618c03d3c026f97e3
Farshid Goudarzi <farshid.goudarzi@gmail.com>
2021-10-06 12:29:25 +0300

    changes from issue_1

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -14,9 +14,15 @@ export const addMessageToStore = (state, payload) => {
 
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+      const convoCopy = { ...convo };
+      const messages = [
+        ...convoCopy.messages,
+        message
+      ];
+      convoCopy.messages = messages;
+      convoCopy.latestMessageText = message.text;
+
+      return convoCopy;
     } else {
       return convo;
     }
@@ -70,10 +76,15 @@ export const addSearchedUsersToStore = (state, users) => {
 export const addNewConvoToStore = (state, recipientId, message) => {
   return state.map((convo) => {
     if (convo.otherUser.id === recipientId) {
-      convo.id = message.conversationId;
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+      const convoCopy = { ...convo };
+      convoCopy.id = message.conversationId;
+      const messages = [
+        ...convoCopy.messages,
+        message
+      ];
+      convoCopy.messages = messages;
+      convoCopy.latestMessageText = message.text;
+      return convoCopy;
     } else {
       return convo;
     }
