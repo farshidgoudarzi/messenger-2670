bd7cdfe6ffeb16f52ae6f367cf4a29df79c37e82
Farshid Goudarzi <farshid.goudarzi@gmail.com>
2021-10-06 12:40:42 +0300

    changes from issue 1

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -111,13 +111,6 @@ router.get("/", async (req, res, next) => {
       conversations[i] = convoJSON;
     }
 
-    // Sort conversation messages:
-    convoJSON.messages?.sort((a, b) => a.createdAt - b.createdAt);
-
-    // set properties for notification count and latest message preview
-    convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length - 1].text;
-    conversations[i] = convoJSON;
-
     res.json(conversations);
   } catch (error) {
     next(error);
